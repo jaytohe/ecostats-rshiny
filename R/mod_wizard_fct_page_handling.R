5f1f97900c52b3dd09990c74ef876dc08a3b3d7a
jaytohe <jaytohe@github.com>
2024-07-23 23:45:09 +0300

    Added some br space between page content and wizard next/prev buttons

--- R/mod_wizard_fct_page_handling.R
+++ R/mod_wizard_fct_page_handling.R
@@ -5,6 +5,9 @@ wrapPage <- function(title, page, button_left = NULL, button_right = NULL) {
       column(12, page)
     ),
     fluidRow(
+      column(12, br(), br()) #add some space between bottom buttons
+    ),
+    fluidRow(
       column(6, button_left),
       column(6, button_right)
     )
