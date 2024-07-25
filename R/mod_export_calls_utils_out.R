000fb65b8e3ca4659a501d6c1cc56da2fb562564
jaytohe <jaytohe@github.com>
2024-07-25 12:29:39 +0300

    Replaced for-loop with map and list_rbind for better performance when creating capture history

--- R/mod_export_calls_utils_out.R
+++ R/mod_export_calls_utils_out.R
@@ -3,23 +3,16 @@
 #' This function reads all the
 #'
 #' @importFrom dplyr select mutate bind_rows
-#' @importFrom purrr walk
+#' @importFrom purrr map list_rbind
 #' @noRd
 create_capture_history <- function(call_groups) {
-  capt_hist <- data.frame()
   ## <Session>, <ID>, <Occasion>, <Detector>, <bearing>, <toa>
   ## where ID is the group_id, Detector is the mic_id
-
-  walk(call_groups, function(call_group) {
-    tmp_df <- call_group$backend_rows %>%
+  map(call_groups, \(call_group) call_group$backend_rows %>%
       # Occasion is hard-coded since ascr does not support multi-occasion data.
       # Session will eventually be supplied by user input; Hard-coded for now.
       mutate(Session = "demo", ID = call_group$group_id, Occasion = as.integer(1), Detector = mic_id, toa = as.numeric(toa)) %>%
       select(Session, ID, Occasion, Detector, bearing, toa) # abide to secr/ascr naming convention
-
-    golem::print_dev(tmp_df)
-    # append rows to main dataframe
-    capt_hist <<- bind_rows(capt_hist, tmp_df)
-  })
-  return(capt_hist)
+  ) %>%
+    list_rbind()
 }
