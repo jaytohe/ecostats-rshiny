3ce4edff7b49262ed244ff84dcf9425885e27e8b
jaytohe <jaytohe@github.com>
2024-08-04 21:10:57 +0100

    Replaced fileInput with shinyFiles

--- R/mod_file_upload.R
+++ R/mod_file_upload.R
@@ -6,21 +6,24 @@
 #'
 #' @noRd
 #'
-#' @importFrom bslib layout_columns
+#' @importFrom shinyFiles shinyFilesButton
+#'
 mod_file_upload_ui <- function(id){
   ns <- NS(id)
   tagList(
     fluidRow(
-      column(6, fileInput(
-        inputId=ns("microphones"),
-        label="Upload microphones coordinates",
-        accept=c("text/csv"),
-        buttonLabel="Upload...")),
-      column(6, fileInput(
-        inputId=ns("recordings"),
-        label="Upload recordings data",
-        accept=c("text/csv"),
-        buttonLabel="Upload..."))
+      column(6, shinyFilesButton(
+        id=ns("microphones"),
+        label="Select microphones coordinates CSV",
+#        accept=c("text/csv"),
+        multiple = FALSE,
+        title="Select CSV file")),
+      column(6, shinyFilesButton(
+        id=ns("recordings"),
+        label="Select recordings CSV",
+#        accept=c("text/csv"),
+        multiple = FALSE,
+        title="Select CSV file"))
     ),
     fluidRow(
       column(4, div(
@@ -41,21 +44,33 @@ mod_file_upload_ui <- function(id){
 #' @noRd
 
 #' @importFrom attempt attempt is_try_error try_catch
+#' @importFrom shinyFiles shinyFileChoose getVolumes parseFilePaths
+#' @importFrom fs path_home
 mod_file_upload_server <- function(id, r){
   moduleServer( id, function(input, output, session){
     ns <- session$ns
+
+    volumes <- c(Home = path_home(),  getVolumes()())
+    shinyFileChoose(input, "microphones", session = session, roots = volumes)
+    shinyFileChoose(input, "recordings", session = session, roots = volumes)
+
     observeEvent(input$microphones, {
-      micData <- attempt(read_csv_vroom(input$microphones$datapath))
+      req(!is.integer(input$microphones))
+      f <- parseFilePaths(volumes, input$microphones)
+      micData <- attempt(read_csv_vroom(f$datapath))
         if(is_try_error(micData)) {
           golem::invoke_js("erroralert", list(title="Microphones CSV read error!", msg=micData))
+        } else {
+          output$tblMics <- renderTable(head(micData))
+          # Add micData to global reactiveValues
+          r$micData <- micData
         }
-        output$tblMics <- renderTable(head(micData))
-        # Add micData to global reactiveValues
-        r$micData <- micData
     })
 
     observeEvent(input$recordings, {
-      recData <- attempt(read_csv_vroom(input$recordings$datapath))
+      req(!is.integer(input$recordings))
+      f <- parseFilePaths(volumes, input$recordings)
+      recData <- attempt(read_csv_vroom(f$datapath))
       if(is_try_error(recData)) {
         golem::invoke_js("erroralert", list(title="Recordings CSV read error!", msg=recData))
       } else {
